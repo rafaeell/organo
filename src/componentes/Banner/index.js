22dbf556de2ad7452376df93654070cd1ac93f4e
Rafael Siqueira <rafael.siqueira@pixter.com.br>
2022-07-18 09:04:05 -0300

    Iniciando repositório

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import './Banner.css';
+
+function Banner() {
+    return (
+        <Fragment>
+            <header className='banner'>
+                <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+            </header>
+            <h1>Organo</h1>
+        </Fragment>
+    );
+}
+
+
+export default Banner;
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,17 @@
+import './CampoTexto.css';
+
+const CampoTexto = (props) => {
+
+    const aoDigitado = (e) => {
+       props.aoAlterado(e.target.value);
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input name={props.name} value={props.valor} onChange={aoDigitado} required={props.obrigatorio} type="text" placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto;
